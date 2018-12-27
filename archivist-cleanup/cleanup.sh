892e2af6ed3cfac25f83cbedf591cc51253ac49b
rmoriz <roland@moriz.de>
2018-12-27 22:31:48 +0100

    now with syncthing

---
+++ archivist-cleanup/cleanup.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+cd /incoming
+
+find . -mmin +15 -type f -print0 \
+  | rsync  \
+      --archive \
+      --verbose \
+      --files-from=- \
+      --from0 \
+      --remove-source-files \
+      /incoming/ /data/
+
+# needs GNU find for -empty
+find . -type d -not -path ".stfolder" -empty -delete
---
+++ archivist-syncthing/README.md
@@ -0,0 +1,6 @@
+config/cert.pem
+config/config.xml
+config/csrftokens.txt
+config/https-cert.pem
+config/https-key.pem
+config/key.pem
